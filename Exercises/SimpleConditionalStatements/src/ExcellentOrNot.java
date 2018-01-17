9129513255df5f64c1ea0dc9626a34aea457f37f
AleksandarBoev <aleksandarboev95@gmail.com>
2018-01-17 23:24:51 +0200

    Simple Conditional statements fill

---
+++ Exercises/SimpleConditionalStatements/src/ExcellentOrNot.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class ExcellentOrNot {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double grade = Double.parseDouble(scanner.nextLine());
+        System.out.println(grade >= 5.5 ? "Excellent!" : "Not excellent.");
+        //code ends here
+    }
+}
